37bb8dddab8e9ae1da748f307ab9a94ba6e1c267
ze.mandkarn@gmail.com <ze.mandkarn@gmail.com>
2015-11-10 15:16:01 +0700

    finish

---
+++ TwoZeroFourEightScoreView.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace twozerofoureight
+{
+    partial class TwoZeroFourEightScoreView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.lblScore2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label.ForeColor = System.Drawing.Color.Red;
+            this.label.Location = new System.Drawing.Point(77, 32);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(142, 52);
+            this.label.TabIndex = 0;
+            this.label.Text = "Score";
+            this.label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblScore2
+            // 
+            this.lblScore2.AutoSize = true;
+            this.lblScore2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.lblScore2.ForeColor = System.Drawing.Color.Red;
+            this.lblScore2.Location = new System.Drawing.Point(119, 118);
+            this.lblScore2.Name = "lblScore2";
+            this.lblScore2.Size = new System.Drawing.Size(42, 46);
+            this.lblScore2.TabIndex = 1;
+            this.lblScore2.Text = "0";
+            this.lblScore2.Click += new System.EventHandler(this.lblScore2_Click);
+            // 
+            // TwoZeroFourEightScoreView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.lblScore2);
+            this.Controls.Add(this.label);
+            this.Name = "TwoZeroFourEightScoreView";
+            this.Text = "TwoZeroFourEightScoreView";
+            this.Load += new System.EventHandler(this.TwoZeroFourEightScoreView_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label lblScore2;
+    }
+}
---
+++ TwoZeroFourEightScoreView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace twozerofoureight
+{
+    public partial class TwoZeroFourEightScoreView : Form,View
+    {
+        Model M;
+        public TwoZeroFourEightScoreView()
+        {
+            InitializeComponent();
+            M = new TwoZeroFourEightModel();
+            M.AttachObserver(this);
+        }
+
+        public void Notify(Model m)
+        {
+            lblScore2.Text = (((TwoZeroFourEightModel)m).Updatescore());
+            
+            //Updatescore(((TwoZeroFourEightModel) m).GetBoard());
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblScore2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TwoZeroFourEightScoreView_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
